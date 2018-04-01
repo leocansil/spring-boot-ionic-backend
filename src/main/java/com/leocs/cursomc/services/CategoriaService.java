51d9f8373b03f1db1ebd4c348aa2d21cd4378f6d
Léo Candido Silva <leocansil@gmail.com>
2018-04-01 10:28:26 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/leocs/cursomc/services/CategoriaService.java
+++ src/main/java/com/leocs/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.leocs.cursomc.domain.Categoria;
 import com.leocs.cursomc.repositories.CategoriaRepository;
+import com.leocs.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -27,7 +28,7 @@ public class CategoriaService {
 	*/
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo : " + Categoria.class.getName()));
 	}
 	
 	/* Notas de Aula
@@ -35,5 +36,20 @@ public class CategoriaService {
 	 Não usar repository.save para salvar lista, utilizar o comando abaixo
 	 
 	 repository.saveAll(Arrays.asList(cat1, cat2))
+	 */
+	
+	/* Notas de Aula
+
+	 Retornar Exceção:
+	 
+	import java.util.Optional;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo : " + Categoria.class.getName()
+			));
+	}
+	 
 	 */
 }
