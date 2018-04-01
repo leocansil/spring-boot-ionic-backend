7042de661e7fd3126b3827bf99a87b98e9313762
Léo Candido Silva <leocansil@gmail.com>
2018-04-01 10:46:35 -0300

    Estado e Cidade

---
+++ src/main/java/com/leocs/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.leocs.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.leocs.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
