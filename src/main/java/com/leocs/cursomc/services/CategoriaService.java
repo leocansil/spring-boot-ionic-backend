63d321b050fd27c000c7ec3dbfffaa285422dc13
Léo Candido Silva <leocansil@gmail.com>
2018-04-01 09:34:24 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/leocs/cursomc/services/CategoriaService.java
@@ -0,0 +1,32 @@
+package com.leocs.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leocs.cursomc.domain.Categoria;
+import com.leocs.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	/* Notas de aula
+	em categoriaService.
+
+	import java.util.Optional;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+	*/
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
