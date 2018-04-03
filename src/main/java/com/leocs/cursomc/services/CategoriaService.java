b299260e2c9a8166fc41969ff5558490337f720a
Léo Candido Silva <leocansil@gmail.com>
2018-04-03 16:51:33 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/leocs/cursomc/services/CategoriaService.java
+++ src/main/java/com/leocs/cursomc/services/CategoriaService.java
@@ -26,7 +26,7 @@ public class CategoriaService {
 	}
 
 	*/
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo : " + Categoria.class.getName()));
 	}
@@ -55,6 +55,11 @@ public class CategoriaService {
 	
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
 		return repo.save(obj);
 	}
 }
