621f8bc6054227c1adc63aef4d870fb6ba5e4ffd
Léo Candido Silva <leocansil@gmail.com>
2018-04-05 11:26:21 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/leocs/cursomc/services/DBService.java
+++ src/main/java/com/leocs/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.leocs.cursomc.domain.PagamentoComCartao;
 import com.leocs.cursomc.domain.Pedido;
 import com.leocs.cursomc.domain.Produto;
 import com.leocs.cursomc.domain.enums.EstadoPagamento;
+import com.leocs.cursomc.domain.enums.Perfil;
 import com.leocs.cursomc.domain.enums.TipoCliente;
 import com.leocs.cursomc.repositories.CategoriaRepository;
 import com.leocs.cursomc.repositories.CidadeRepository;
@@ -129,14 +130,22 @@ public class DBService {
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "leocansil@gmail.com", "31628382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		
+		cli2.getTelefones().addAll(Arrays.asList("9388321", "343252625"));
+
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
 				
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "28777012", cli2, c2);
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 		
-		clientepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		clientepository.saveAll(Arrays.asList(cli1,cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2,e3));
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
