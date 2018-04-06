9100121ad4aee60a14cd685769101383c977f512
Léo Candido Silva <leocansil@gmail.com>
2018-04-06 11:22:02 -0300

    Usando padrao de nomes para imagens

--- src/main/java/com/leocs/cursomc/domain/Cliente.java
+++ src/main/java/com/leocs/cursomc/domain/Cliente.java
@@ -55,9 +55,6 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	
-	private String imageUrl;
-	
 	public Cliente() {
 		addPerfil(Perfil.CLIENTE);
 	}
@@ -177,14 +174,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-	
 	
 }
